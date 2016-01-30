03b7939d22586c353d200a657768bf698aed9013
Mandur <mikhail.chatillon@hotmail.com>
2016-01-30 15:01:32 +0100

    On ajoute la faune

---
+++ Assets/Script/FlyingShitScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingShitScript : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(speed,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	       
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Poulet")
+        {
+            this.transform.Rotate(new Vector3(0,0,-30));
+            this.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+    }
+}
