f69ac24c038e440e8099ba8886717a896b6ffe3d
Francois-Xavier Aeberhard <fx@red-agent.com>
2016-01-30 13:29:20 +0100

    Mon premier commit

---
+++ Assets/Script/SpawnController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnController : MonoBehaviour {
+    
+    public Rigidbody2D characters;
+    public int spawnEvery = 5;
+
+    // Use this for initialization
+    void Start () {
+
+        InvokeRepeating("Spawn", spawnEvery, spawnEvery);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void Spawn()
+    {
+
+          Rigidbody2D c = (Rigidbody2D)Instantiate(characters, transform.position, transform.rotation);
+    }
+}
